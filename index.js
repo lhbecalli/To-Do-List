efdd2f4828803c6395a1a2ca42322b737e3087c5
lhbecalli <luizhenriquebecallidefaria@gmail.com>
2025-11-13 16:28:10 -0300

    Changes to JS and CSS

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 const button = document.querySelector('.button')
 const input = document.querySelector('.input')
+const clear = document.querySelector('.clear')
+const ul = document.querySelector('.list')
 
+clear.style.display = 'none'
 
-function evento(){
-    const value = input.value.trim();
-    if(value===''){
-        return;
-    }
-
-    const ul = document.querySelector('.list')
-    const li = document.createElement('li')
-
-    li.innerHTML = value + '<span class="check">V</span> <span class="delete">X</span>'
+function atualizarBotao() {
     
-/* Deletar os valores */
-    ul.appendChild(li);
-    const delet = li.querySelector('.delete')
-
-delet.addEventListener('click', ()=>{
-    li.remove()
-})
-
-/* Check nos valores */
-    const check = li.querySelector('.check');
-check.addEventListener('click', ()=>{
-    li.classList.toggle('done')
-    })
-
+    clear.style.display = ul.children.length > 0 ? 'block' : 'none'
 }
 
-/* Adicionar tarefa */
+function evento() {
+    const value = input.value.trim();
+    if(value === '') return;
+
+    const li = document.createElement('li')
+    li.innerHTML = value + ' <span class="check">V</span> <span class="delete">X</span>'
+
+    
+    ul.appendChild(li);
+
+    
+    const delet = li.querySelector('.delete')
+    delet.addEventListener('click', () => {
+        li.remove()
+        atualizarBotao() 
+    })
+
+
+    const check = li.querySelector('.check')
+    check.addEventListener('click', () => {
+        li.classList.toggle('done')
+    })
+
+    // Limpar tudo
+    clear.addEventListener('click', () => {
+        ul.innerHTML = ''
+        input.value = ''
+        input.focus()
+        atualizarBotao() 
+    })
+
+    input.value = ''
+    input.focus()
+
+    atualizarBotao() 
+}
+
+
 button.addEventListener('click', evento)
 
 
-/* Ao pressionar Enter, registra a tarefa */
-input.addEventListener('keydown', e =>{
-
+input.addEventListener('keydown', e => {
     if(e.key === "Enter"){
         evento()
     }
